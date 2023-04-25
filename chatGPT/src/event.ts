b82dc4eb15c6742488e3385739a793be5f5b4fcf
Raider <gabrielnae1@gmail.com>
2023-04-25 19:49:04 +0200

    s

--- chatGPT/src/event.ts
+++ chatGPT/src/event.ts
@@ -1,3 +1,4 @@
 import { classe } from "./main";
 
 classe.document.buttonSub.addEventListener("click", function() {classe.verifieurTexte()})
+//classe.document.textArea.addEventListener("keyup", function() {classe.TextareaAutoExpand()})
--- chatGPT/src/main.ts
+++ chatGPT/src/main.ts
@@ -13,6 +13,7 @@ class indexHTML {
 
   constructor(HTMLType: HTMLType) {
     this._document = HTMLType
+    this._document.textArea.value = ""
   }
 
   get document(): HTMLType {
@@ -34,20 +35,31 @@ class indexHTML {
     else {
       const loadingText = "Chargement en cours...";
       this.document.divChat.innerHTML = loadingText;
-      this.message.push(`Utilisateur : ${this.document.textArea.value}`);
-      this._prompt += "User: " + this.document.textArea.value + "\n"
+      this.message.push(this.document.textArea.value);
+      this._prompt += this.document.textArea.value + "\n"
       generateText(this._prompt)
       .then((text) => {
         this.document.divChat.innerHTML = text
-        this._prompt += "System: " + text + "\n"
+        this._prompt += text + "\n"
         this.message.push(`Chatbot : ${text}`);
         console.log(this.message)
+        console.log(this._prompt)
+        console.log(text)
+
       })
       .catch((error) => {
         console.error(error);
       });
     }
   }
+
+  /*TextareaAutoExpand(): void {
+    let tailleInitiale = 20
+    if (this.document.textArea.textLength % 104 === 0) {
+      console.log(Math.round((this.document.textArea.textLength/104)+1)*tailleInitiale)
+      this._document.textArea.style.maxHeight = String(Math.round((this.document.textArea.textLength/104)+1)*tailleInitiale) + "px"
+    }
+  }*/
 }
 
 export let classe = new indexHTML({
